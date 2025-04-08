e794f31a300593cece4528fb2f60d8e0166d27f8
Adesomoju <adesomojufisayo@outlook.com>
2025-04-08 19:25:35 +0100

    Initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    googleMapsApiKey: 'your-production-api-key-here'  //  production API key 
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    googleMapsApiKey: 'AIzaSyBGtm6EN7lcdTPbRTAenO2GOPgqfD7j6fU'  // test API key 
+};
